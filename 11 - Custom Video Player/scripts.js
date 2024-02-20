290ad86c0149e30524a41e11fc1be297e294ac03
tli533 <tli533@aucklanduni.ac.nz>
2024-02-20 12:16:09 +1100

    Ex 11 setting consts

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,14 @@
+/*get our elements*/
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+
+const toggle = document.querySelector('.toggle');
+const ranges = document.querySelector('.player__slider');
+const skipButtons = document.querySelector('[data-skip]');
+
+
+/*build out functions*/
+
+/*hook up event listeners*/
